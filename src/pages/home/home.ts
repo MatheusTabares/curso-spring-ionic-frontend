e0b63c367fc57ddd3a44862729af54470cf628d0
MatheusTabares <tabares.lopes@gmail.com>
2018-10-20 18:23:16 -0300

    Obtendo os dados do usuario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()//MT:Habilita a referência da página pelo nome da classe entre 'aspas'
 @Component({
@@ -8,6 +9,11 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
   templateUrl: 'home.html'//MT:Referência para qual arquivo HTML este controller esta controlando
 })
 export class HomePage {
+
+  creds : CredenciaisDTO = {
+    email : "",
+    senha : ""
+  }
 
   constructor(public navCtrl: NavController, public menu:MenuController) {
 
@@ -22,6 +28,7 @@ export class HomePage {
   }
 
   login() {
+    console.log(this.creds);
     //MT: navegar entre páginas
     //MT: o método 'setRoot' não empilha páginas, o método 'push' empilha páginas
     this.navCtrl.setRoot('CategoriasPage');
